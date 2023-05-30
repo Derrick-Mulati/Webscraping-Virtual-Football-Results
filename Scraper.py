08bb15020cc813b0d7f9be68cc9df2fe6d7cc433
Derrick-Mulati <mulatiderrick@gmail.com>
2023-05-30 11:47:55 +0300

    Changes Done

--- Scraper.py
+++ Scraper.py
@@ -3,18 +3,18 @@ import requests
 
 
 
-
-
-
-
-
-
 try:
 
     source = requests.get('https://www.betika.com/en-ke/virtuals/table/6')
     source.raise_for_status()
     soup = BeautifulSoup(source.text, 'html.parser')
-    print(soup)
+
+
+
+    results = soup.find('tbody').find_all('tr')
+
+
+    print(len(results))
  
 except Exception as e:
     print(e)
